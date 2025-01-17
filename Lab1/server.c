995aea57dc87b34bfa2d3c5b3830559432e9514e
hzhang2422 <hzhang2422@gmail.com>
2025-01-16 23:59:14 -0800

    Enhancements to the server code

--- Lab1/server.c
+++ Lab1/server.c
@@ -11,17 +11,30 @@ void error(char *msg)
     exit(1);
 }
 
+void dostuff(int sock)
+{
+    int n;
+    char buffer[256];
+    bzero(buffer, 256);
+    n = read(sock, buffer, 255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n", buffer);
+    n = write(sock, "I got your message", 18);
+    if (n < 0) error("ERROR writing to socket");
+}
+
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
+   
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
+   
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -29,19 +42,27 @@ int main(int argc, char *argv[])
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
+   
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
+   
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
+   
+     while (1) {
+          newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
+          if (newsockfd < 0)
+              error("ERROR on accept");
+        
+          pid = fork();
+          if (pid < 0)
+              error("ERROR on fork");
+        
+          close(sockfd);
+          dostuff(newsockfd);
+          exit(0);
+     }
+   
      return 0; 
 }
