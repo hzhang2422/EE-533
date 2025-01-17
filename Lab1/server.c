d1958146a448270ff74fa918b75f900c2e42a042
Haoyang Zhang <hzhang2422@gmail.com>
2025-01-17 11:47:54 -0800

    Enhancements Modified

--- Lab1/server.c
+++ Lab1/server.c
@@ -1,6 +1,7 @@
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
-#include <stdio.h>
+#include "stdio.h"
+#include <stdlib.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -25,44 +26,48 @@ void dostuff(int sock)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
-     struct sockaddr_in serv_addr, cli_addr;
+    int sockfd, newsockfd, portno, clilen, pid;
+    struct sockaddr_in serv_addr, cli_addr;
    
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
    
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	 
-     if (sockfd < 0) 
+    if (sockfd < 0) 
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
    
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0) 
+            error("ERROR on binding");
    
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
    
-     while (1) {
-          newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
-          if (newsockfd < 0)
-              error("ERROR on accept");
-        
-          pid = fork();
-          if (pid < 0)
-              error("ERROR on fork");
-        
-          close(sockfd);
-          dostuff(newsockfd);
-          exit(0);
-     }
+    while (1) 
+	{
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen); 
+			if (newsockfd < 0)
+				error("ERROR on accept"); 
+			pid = fork(); 
+			if (pid < 0) 
+				error("ERROR on fork"); 
+			if (pid == 0) 
+			{ 
+		close(sockfd); 
+		dostuff(newsockfd); 
+		exit(0); 
+		} 
+		else 
+			close(newsockfd); 
+		}
    
-     return 0; 
+    return 0; 
 }
